5c1fe6fea203e67aa9b2964c85e4e1a84e1aff54
Daniel Agoziem <stefanpongrz@gmail.com>
2020-12-10 15:45:44 +0100

    minor change

--- src/Views/layout/Navbar.js
+++ src/Views/layout/Navbar.js
@@ -1,6 +1,6 @@
 const Navbar = () => {
     return(
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light px-5">
   <a className="navbar-brand" href="/">Powernik</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
