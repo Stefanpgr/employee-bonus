d77211b99a9db0ee465ddafca2bc5a8dab4da0bb
Daniel Agoziem <stefanpongrz@gmail.com>
2020-12-10 15:43:10 +0100

    minor change

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CreateReport from './Views/CreateReport';
 import Report from "./Views/Report";
 import './App.css';
 import ViewReport from "./Views/ViewReport";
+import Navbar from "./Views/layout/Navbar";
 
 function App() {
   return (
@@ -11,7 +12,9 @@ function App() {
    
          
     <div className="App">
+    <Navbar />
     <Switch>
+    
     <Route exact path="/" component={CreateReport} />
             <Route exact path="/report" component={Report} />
             <Route exact path="/report/:name" component={ViewReport} />
